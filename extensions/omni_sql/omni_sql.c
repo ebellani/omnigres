f3d3c33d4d2ec3e62166505342ac357a594e98a3
Eduardo Bellani <ebellani@gmail.com>
2024-07-23 13:52:45 -0300

    print every value

--- extensions/omni_sql/omni_sql.c
+++ extensions/omni_sql/omni_sql.c
@@ -11,6 +11,9 @@
 
 #include "omni_sql.h"
 
+#include "executor/spi.h"
+#include "utils/jsonb.h"
+
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(statement_in);
@@ -560,4 +563,57 @@ Datum statement_type(PG_FUNCTION_ARGS) {
   }
 
   PG_RETURN_NULL();
+}
+
+PG_FUNCTION_INFO_V1(execq);
+
+Datum execq(PG_FUNCTION_ARGS)
+{
+  char *command;
+  int cnt;
+  int ret;
+  uint64 proc;
+
+  /* Convert given text object to a C string */
+  command = text_to_cstring(PG_GETARG_TEXT_PP(0));
+  cnt = PG_GETARG_INT32(1);
+
+  SPI_connect();
+
+  ret = SPI_exec(command, cnt);
+
+  proc = SPI_processed;
+  Jsonb *jb;
+  /*
+   * If some rows were fetched, print them via elog(INFO).
+   * Try to transform them into json before printing.
+   */
+  if (ret > 0 && SPI_tuptable != NULL)
+    {
+      SPITupleTable *tuptable = SPI_tuptable;
+      TupleDesc tupdesc = tuptable->tupdesc;
+      int natts =  tupdesc->natts;
+      uint64 cardinality;
+      /* loop for each tuple */
+      uint64 j;
+      for (j = 0; j < tuptable->numvals; j++)
+        {
+          HeapTuple tuple = tuptable->vals[j];
+          int i;
+          /* loop for each attribute */
+          for (i = 1; i <= tupdesc->natts; i++) {
+            char *attr_name = SPI_fname(tupdesc, i);
+            char *attr_value = SPI_getvalue(tuple, tupdesc, i);
+
+            elog(INFO, "Tuple attribute name: %s.", attr_name);
+            elog(INFO, "Tuple attribute value: %s.", attr_value);
+          }
+          elog(INFO, "\n");
+        }
+    }
+
+  SPI_finish();
+  pfree(command);
+
+  PG_RETURN_INT64(proc);
 }

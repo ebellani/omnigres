453b64e6b5fe4fe8d40f90a4db8d4262d6d58342
Eduardo Bellani <ebellani@gmail.com>
2024-08-09 15:21:06 -0300

    Problem: Retries can perform poorly under high contention.
    > The simplest of these contention cases is when a whole lot of clients
    > start at the same time, and try to update the same database row. With
    > one client guaranteed to succeed every round, the time to complete all
    > the updates grows linearly with contention ... N clients compete in
    > the first round, N-1 in the second round, and so on. Having every
    > client compete in every round is wasteful. [1]
    
    Solution: Add backoff with jitter to spread the calls over time,
    minimizing contention.
    
    [1] https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/

--- extensions/omni_txn/omni_txn.c
+++ extensions/omni_txn/omni_txn.c
@@ -1,4 +1,5 @@
 // clang-format off
+#include "c.h"
 #include <postgres.h>
 #include <fmgr.h>
 // clang-format on
@@ -9,10 +10,35 @@
 #if PG_MAJORVERSION_NUM < 15
 #include <access/xact.h>
 #endif
+#include <common/pg_prng.h>
 
 PG_MODULE_MAGIC;
 
 static int32 retry_attempts = 0;
+static float8 cap_sleep_secs = 0.0001;    // 100 microseconds
+static float8 base_sleep_secs = 0.000001; // microsecond
+
+/**
+ * The backoff should increase with each attempt.
+ */
+static float8 get_backoff(float8 cap, float8 base, int32 attempt) {
+  float8 exp = (float8)(1 << attempt);
+  return Min(cap, base * exp);
+}
+
+/**
+ * Get the random jitter to avoid contention in the backoff. Uses the
+ * process seed initialized in `InitProcessGlobals`.
+ */
+static float8 get_jitter() { return pg_prng_double(&pg_global_prng_state); }
+
+/**
+ * Implements the backoff + fitter approach
+ * https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+ */
+static float8 backoff_jitter(float8 cap, float8 base, int32 attempt) {
+  return get_jitter() * get_backoff(cap, base, attempt);
+}
 
 PG_FUNCTION_INFO_V1(retry);
 
@@ -30,13 +56,11 @@ Datum retry(PG_FUNCTION_ARGS) {
 
   text *stmts = PG_GETARG_TEXT_PP(0);
   char *cstmts = text_to_cstring(stmts);
-
   bool retry = true;
   retry_attempts = 0;
   while (retry) {
     XactIsoLevel = XACT_SERIALIZABLE;
     SPI_connect_ext(SPI_OPT_NONATOMIC);
-
     MemoryContext current_mcxt = CurrentMemoryContext;
     PG_TRY();
     {
@@ -51,10 +75,6 @@ Datum retry(PG_FUNCTION_ARGS) {
       ErrorData *err = CopyErrorData();
       if (err->sqlerrcode == ERRCODE_T_R_SERIALIZATION_FAILURE) {
         ereport(NOTICE, errmsg("%d", max_attempts));
-        if (++retry_attempts > max_attempts) {
-          ereport(ERROR, errcode(err->sqlerrcode),
-                  errmsg("maximum number of retries (%d) has been attempted", max_attempts));
-        }
         PopActiveSnapshot();
         AbortCurrentTransaction();
 
@@ -62,6 +82,13 @@ Datum retry(PG_FUNCTION_ARGS) {
         StartTransactionCommand();
         PushActiveSnapshot(GetTransactionSnapshot());
 
+        if (++retry_attempts > max_attempts) {
+          ereport(ERROR, errcode(err->sqlerrcode),
+                  errmsg("maximum number of retries (%d) has been attempted", max_attempts));
+        } else {
+          DirectFunctionCall1(pg_sleep,
+                              Float8GetDatum(backoff_jitter(cap_sleep_secs, base_sleep_secs, retry_attempts)));
+        }
       } else {
         retry_attempts = 0;
         PG_RE_THROW();
